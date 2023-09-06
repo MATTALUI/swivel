527b0740019e4ed832fab1e6760e335a00ee0bfd
Matt Hummer <biggreyson@gmail.com>
2023-09-05 21:17:02 -0600

    add some more interesting defaults

--- ui/assets/js/AnimationObject.js
+++ ui/assets/js/AnimationObject.js
@@ -1,9 +1,6 @@
 class AnimationObject {
   constructor() {
     this.root = new ObjectNode();
-    const child = new ObjectNode();
-    this.root.appendChild(child);
-    child.setPosition(new Vec2(0.5, 0.25));
   }
 
   clone() {
--- ui/assets/js/Frame.js
+++ ui/assets/js/Frame.js
@@ -1,25 +1,78 @@
 class Frame {
-  constructor () {
+  constructor() {
     this.previewImage = null;
-    this.objects = [
-      new AnimationObject(),
-      new AnimationObject(),
-    ];
-
-    this.objects[1].root.setPosition(new Vec2(0.25, 0.5));
-    this.objects[1].root.children[0].setPosition(new Vec2(0.25, 0.25));
-
-    this.objects[1].root.children[0].appendChild(new ObjectNode());
-    this.objects[1].root.children[0].children[0].setPosition(new Vec2(0, 0));
-
-    this.objects[1].root.appendChild(new ObjectNode());
-    this.objects[1].root.children[1].setPosition(new Vec2(0.12, 0.89));
+    this.objects = Frame.buildDefaultObjects();
   }
 
-  clone () {
+  clone() {
     const clone = new Frame();
     clone.objects = this.objects.map(o => o.clone());
 
     return clone;
   }
+}
+
+// This realistically is only used when initializing the first empty frame for a
+// blank object.
+Frame.buildDefaultObjects = () => {
+  let child, newestChild;
+  const objects = [];
+  // M
+  const m = new AnimationObject();
+  m.root.setPosition(new Vec2(0.1, 0.69));
+  child = new ObjectNode();
+  m.root.appendChild(child);
+  child.setPosition(new Vec2(0.1, 0.5));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  child = newestChild;
+  child.setPosition(new Vec2(0.15, 0.58));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  child = newestChild;
+  child.setPosition(new Vec2(0.2, 0.5));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  child = newestChild;
+  child.setPosition(new Vec2(0.2, 0.69));
+  // A
+  const a = new AnimationObject();
+  a.root.setPosition(new Vec2(0.25, 0.69));
+  child = new ObjectNode();
+  a.root.appendChild(child);
+  child.setPosition(new Vec2(0.275, 0.5));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  child = newestChild;
+  child.setPosition(new Vec2(0.3, 0.69));
+  // T
+  const t1 = new AnimationObject();
+  t1.root.setPosition(new Vec2(0.375, 0.69));
+  child = new ObjectNode();
+  t1.root.appendChild(child);
+  child.setPosition(new Vec2(0.375, 0.5));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  newestChild.setPosition(new Vec2(0.35, 0.5));
+  newestChild = new ObjectNode()
+  child.appendChild(newestChild);
+  newestChild.setPosition(new Vec2(0.4, 0.5));
+  // T
+  const t2 = new AnimationObject();
+  t2.root.setPosition(new Vec2(0.475, 0.5));
+
+
+  objects.push(m, a, t1, t2);
+  child = new ObjectNode();
+  t2.root.appendChild(child);
+  child.setPosition(new Vec2(0.45, 0.5));
+  child = new ObjectNode();
+  t2.root.appendChild(child);
+  child.setPosition(new Vec2(0.5, 0.5));
+  child = new ObjectNode();
+  t2.root.appendChild(child);
+  child.setPosition(new Vec2(0.475, 0.69));
+  
+
+  return objects;
 }
