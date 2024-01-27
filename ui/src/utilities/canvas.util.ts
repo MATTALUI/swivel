67b60c8d039b5cf0b481577cffa26af46d6f35c9
Matt Hummer <biggreyson@gmail.com>
2024-01-27 14:28:51 -0700

    #81: render image nodes in object creator

--- ui/src/utilities/canvas.util.ts
+++ ui/src/utilities/canvas.util.ts
@@ -168,23 +168,65 @@ export const drawAnimationObjectToCanvas = (
   if (!ctx) throw new Error("Context unavailable");
   // Clear it out
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const resourcesById = globalState.mediaResources.byId || {};
 
   const allControlNodes: ObjectNode[] = [];
   const connectNodeToChildren = (node: ObjectNode, controllable = true) => {
     node.children.forEach((child) => {
       if (child.children.length) connectNodeToChildren(child, controllable);
       if (controllable) allControlNodes.push(child); // Children nodes
-      const { x: startX, y: startY } = child.position.getRenderedPosition(ctx.canvas.width, ctx.canvas.height);
-      const { x: endX, y: endY } = node.position.getRenderedPosition(ctx.canvas.width, ctx.canvas.height);
-      const alpha = controllable ? 1 : 0.5;
+      const { x: parentX, y: parentY } = child.position.getRenderedPosition(ctx.canvas.width, ctx.canvas.height);
+      const { x: childX, y: childY } = node.position.getRenderedPosition(ctx.canvas.width, ctx.canvas.height);
+      switch (child.type) {
+      case ObjectNodeTypes.IMAGE: {
+        if (!resourcesById) return; // Still waiting on resources
+        const imageResource = resourcesById[child.image || "🦖"];
+        if (!imageResource || !imageResource.element) {
+          if (!child.width || !child.height) {
+            ctx.fillStyle = "#3E2F5B33";
+            ctx.rect(parentX, parentY, childX - parentX, childY - parentY);
+            ctx.fill();
+            return;
+          }
+        }
 
-      ctx.beginPath();
-      ctx.strokeStyle = `rgba(0,0,0,${alpha})`;
-      ctx.moveTo(startX, startY);
-      ctx.lineTo(endX, endY);
-      ctx.lineWidth = child.size;
-      ctx.lineCap = "round";
-      ctx.stroke();
+        const imageWidth = imageResource?.width || child.width || 1;
+        const imageHeight = imageResource?.height || child.height || 1;
+        const imageDiagonal = getPositionDistance(0, 0, imageWidth, imageHeight);
+        const renderedDiagonal = getPositionDistance(parentX, parentY, childX, childY);
+        const renderedWidth = imageWidth * renderedDiagonal / imageDiagonal;
+        const renderedHeight = imageHeight * renderedDiagonal / imageDiagonal;
+        const naturalRotation = 360 - radToDeg(Math.atan(imageHeight / imageWidth));
+        const actualRotation = 360 - getAngleOfChange(parentX, parentY, childX, childY);
+        const angleDifferential = naturalRotation - actualRotation;
+
+        if (!controllable) ctx.globalAlpha = 0.5;
+        // Move to the parent node for easier drawing
+        ctx.translate(parentX, parentY);
+        ctx.rotate(degToRad(angleDifferential));
+        ctx.fillStyle = "#3E2F5B33";
+        ctx.rect(0, 0, renderedWidth, renderedHeight);
+        ctx.fill();
+        if (imageResource?.element)
+          ctx.drawImage(imageResource.element, 0, 0, renderedWidth, renderedHeight);
+        // Move back to origin
+        ctx.rotate(-degToRad(angleDifferential));
+        ctx.translate(-parentX, -parentY);
+        ctx.globalAlpha = 1.0;
+        break;
+      }
+      default: {
+        const alpha = controllable ? 1 : 0.5;
+
+        ctx.beginPath();
+        ctx.strokeStyle = `rgba(0,0,0,${alpha})`;
+        ctx.moveTo(parentX, parentY);
+        ctx.lineTo(childX, childY);
+        ctx.lineWidth = child.size;
+        ctx.lineCap = "round";
+        ctx.stroke();
+      }
+      }
 
     });
   };
